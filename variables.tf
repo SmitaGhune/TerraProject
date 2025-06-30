6a5d9cc08f3eb83e5e26de2bfa899b19c15bad40
Smita Ghune <72501177+9607491581@users.noreply.github.com>
2025-06-30 18:03:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,6 @@ variable "location" {
 
 variable "resource_group_name" {
   type    = string
-  default = "rg-terraform"
 }
 
 variable "storage_account_name" {
@@ -14,3 +13,53 @@ variable "storage_account_name" {
 }
 
 variable "subscription_id" {}
+variable "location" {
+  type        = string
+  description = "Azure location for all resources"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of the virtual network"
+}
+
+variable "vnet_address_space" {
+  type        = string
+  description = "Address space for the virtual network"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the subnet"
+}
+
+variable "subnet_address_prefix" {
+  type        = string
+  description = "Subnet address prefix"
+}
+
+variable "nic_name" {
+  type        = string
+  description = "Name of the network interface"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of the virtual machine"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Size of the virtual machine"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the VM"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Admin password for the VM"
+  sensitive   = true
+}
